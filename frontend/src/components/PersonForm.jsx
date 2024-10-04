4231b5d3c20f4826f576b28afe859dac460ec13e
sgunnell <34221382+sgunnell@users.noreply.github.com>
2024-10-04 12:44:54 -0700

    Add files via upload

---
+++ frontend/src/components/PersonForm.jsx
@@ -0,0 +1,26 @@
+
+const PersonForm = (props) => {
+
+    return(
+
+        <form onSubmit={props.addName}>
+        <div>
+          name: <input 
+            value ={props.name}
+            onChange ={props.handleNameChange}
+          />
+        </div>
+        <div>
+          number: <input 
+            value ={props.number}
+            onChange ={props.handleNumberChange}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+    )
+}
+
+export default PersonForm
